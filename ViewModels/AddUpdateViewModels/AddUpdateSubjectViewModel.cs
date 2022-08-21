cbf8981769dd9946008c0f13ccf10184e7ec190f
Pieter-stack <pieterven12@gmail.com>
2022-08-21 08:59:07 +0200

    Subjects page CRUD completed and Search functionality started ( Decided not to use popups with maui on a windows machine)
    Subjects page CRUD completed and Search functionality started ( Decided not to use popups with maui on a windows machine)

--- ViewModels/AddUpdateViewModels/AddUpdateSubjectViewModel.cs
+++ ViewModels/AddUpdateViewModels/AddUpdateSubjectViewModel.cs
@@ -1,17 +1,74 @@
 ﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EduCube.Models;
+using EduCube.Services;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EduCube.ViewModels.AddUpdateViewModels
-{
+{ 
+    [QueryProperty(nameof(SubjectDetail), "SubjectDetail")]
 
-    public partial class AddUpdateSubjectViewModel 
+
+    public partial class AddUpdateSubjectViewModel : ObservableObject
     {
-       
+
+        [ObservableProperty]
+        public SubjectModel _subjectDetail = new SubjectModel();
+
+            private readonly ISubjectService _subjectRepository;
+            public AddUpdateSubjectViewModel(ISubjectService subjectService)
+            {
+            _subjectRepository = subjectService;
+            }
 
 
+            //add display action to assign active state
+            [ICommand]
+            public async void AddUpdateSubject()
+            {
+            int response = -1;
+            if (SubjectDetail.SubjectID > 0)
+            {
+                response = await _subjectRepository.EditSubject(SubjectDetail);
+            }
+            else
+            {
+                response = await _subjectRepository.AddSubject(new Models.SubjectModel
+                {
+                    SubjectTitle = SubjectDetail.SubjectTitle,
+                    SubjectCode = SubjectDetail.SubjectCode,
+                    SubjectLecturer = SubjectDetail.SubjectLecturer,
+                    SubjectDescription = SubjectDetail.SubjectDescription,
+                    SubjectCredits = SubjectDetail.SubjectCredits,
+                    SubjectPrice = SubjectDetail.SubjectPrice,
+                    SubjectDate = SubjectDetail.SubjectDate,
+                    SubjectTime = SubjectDetail.SubjectTime,
+                    SubjectImage = SubjectDetail.SubjectImage,
+                    SubjectHours = SubjectDetail.SubjectHours,
+                    SubjectVenue = SubjectDetail.SubjectVenue
+                });
+            }
+
+                if (response > 0)
+                {
+                    await Shell.Current.DisplayAlert("subject info saved", "Record Saved", "OK");
+                    //navigate back!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Not added", "Something went wrong while adding record", "OK");
+                }
+
+            }
+            
+
+        }
     }
-}
+
--- Views/SubjectsPage.xaml.cs
+++ Views/SubjectsPage.xaml.cs
@@ -1,21 +1,32 @@
 using EduCube.ViewModels;
+using EduCube.ViewModels.AddUpdateViewModels;
 
 namespace EduCube;
 
 public partial class SubjectsPage : ContentPage
 {
-	//create link for viewmodel
+	//link viewmodel to view
 	private SubjectViewModel _viewModel;
 
     //pass viewmodel
     public SubjectsPage(SubjectViewModel vm)
 	{
 		InitializeComponent();
-		//link viewmodel
         _viewModel = vm;	
         //bind viewmodel
         this.BindingContext = vm;
 	}
 
+    protected override void OnAppearing()
+    {
+        //run getsubjects when page loads
+        base.OnAppearing();
+        _viewModel.GetSubjectListCommand.Execute(null);
+    }
+
 
 }
+
+
+
+
